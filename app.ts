6f29aa8e21ddc6ac280108a992cc481fdf4eff19
Thoai1503 <vothoai1503@gmail.com>
2025-10-09 23:22:58 +0700

    my commit

--- app.ts
+++ app.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { type Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: ["https://electric-commercial.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (_, res: Response) => {
+  res.send("API is running....");
 });
 
 if (process.env.NODE_ENV !== "production") {
--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,20 @@
-import express from "express";
+import express, {} from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(cors({
+    origin: ["https://electric-commercial.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
+app.use(cookieParser());
+//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (_, res) => {
+    res.send("API is running....");
 });
 if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
