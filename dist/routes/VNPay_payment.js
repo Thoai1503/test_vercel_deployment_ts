ebe72b8956a1057fad5bebbee25d107b794c287c
Thoai1503 <vothoai1503@gmail.com>
2025-10-20 15:27:06 +0700

    fix 3

--- dist/routes/VNPay_payment.js
+++ dist/routes/VNPay_payment.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 import express, {} from "express";
 import VNPay from "../service/VNPay.js";
+import MoMo from "../service/MoMo.js";
 const router = express.Router();
 function getClientIp(req) {
     return (req.headers["x-forwarded-for"] ||
@@ -49,6 +50,44 @@ router.get("/vnpay_return", async (req, res) => {
     catch (e) {
         console.error("Error verifying VNPAY return:", e);
         return res.status(500).json({ error: "Payment verification failed" });
+    }
+});
+router.post("/create_payment", async (req, res) => {
+    const orderId = req.body?.orderId || `${Date.now()}`;
+    const amount = req.body?.amount || 100000;
+    const { method } = req.body;
+    switch (method) {
+        case "vnpay":
+            const vnpService = new VNPay({
+                tmnCode: process.env.vnp_TmnCode,
+                hashSecret: process.env.vnp_HashSecret,
+                vnpUrl: process.env.vnp_Url,
+                returnUrl: process.env.vnp_ReturnUrl,
+            });
+            const payment = await vnpService.createPayment(orderId, amount, {
+                ipAddress: getClientIp(req),
+                orderInfo: req.body?.orderInfo,
+                bankCode: req.body?.bankCode,
+                orderType: req.body?.orderType,
+                locale: req.body?.language,
+            });
+            return res.status(200).json({ url: payment.paymentUrl });
+        case "momo":
+            const momoService = new MoMo({
+                partnerCode: process.env.momo_PartnerCode,
+                accessKey: process.env.momo_AccessKey,
+                secretKey: process.env.momo_SecretKey,
+                redirectUrl: process.env.momo_RedirectUrl,
+                ipnUrl: process.env.momo_IpnUrl,
+                endpoint: process.env.momo_Endpoint,
+            });
+            const result = await momoService.createPayment(orderId, amount, {
+                requestType: req.body?.requestType,
+                orderInfo: req.body?.orderInfo,
+                extraData: req.body?.extraData,
+                lang: req.body?.lang,
+            });
+            return res.status(200).json({ url: result.payUrl });
     }
 });
 export default router;
--- routes/VNPay_payment.ts
+++ routes/VNPay_payment.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 import express, { type Request, type Response } from "express";
 import VNPay from "../service/VNPay.js";
+import MoMo from "../service/MoMo.js";
 
 const router = express.Router();
 
@@ -54,6 +55,45 @@ router.get("/vnpay_return", async (req: Request, res: Response) => {
   } catch (e) {
     console.error("Error verifying VNPAY return:", e);
     return res.status(500).json({ error: "Payment verification failed" });
+  }
+});
+router.post("/create_payment", async (req: Request, res: Response) => {
+  const orderId = (req.body?.orderId as string) || `${Date.now()}`;
+  const amount = (req.body?.amount as number) || 100000;
+  const { method } = req.body;
+  switch (method) {
+    case "vnpay":
+      const vnpService = new VNPay({
+        tmnCode: process.env.vnp_TmnCode,
+        hashSecret: process.env.vnp_HashSecret,
+        vnpUrl: process.env.vnp_Url,
+        returnUrl: process.env.vnp_ReturnUrl,
+      });
+      const payment = await vnpService.createPayment(orderId, amount, {
+        ipAddress: getClientIp(req),
+        orderInfo: req.body?.orderInfo,
+        bankCode: req.body?.bankCode,
+        orderType: req.body?.orderType,
+        locale: req.body?.language,
+      });
+      return res.status(200).json({ url: payment.paymentUrl });
+
+    case "momo":
+      const momoService = new MoMo({
+        partnerCode: process.env.momo_PartnerCode!,
+        accessKey: process.env.momo_AccessKey!,
+        secretKey: process.env.momo_SecretKey!,
+        redirectUrl: process.env.momo_RedirectUrl!,
+        ipnUrl: process.env.momo_IpnUrl!,
+        endpoint: process.env.momo_Endpoint!,
+      });
+      const result = await momoService.createPayment(orderId, amount, {
+        requestType: req.body?.requestType,
+        orderInfo: req.body?.orderInfo,
+        extraData: req.body?.extraData,
+        lang: req.body?.lang,
+      });
+      return res.status(200).json({ url: result.payUrl });
   }
 });
 
