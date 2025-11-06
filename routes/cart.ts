ba9f50f51c13d97d8d24126d39b96b94a751da82
Thoai1503 <vothoai1503@gmail.com>
2025-11-06 20:31:32 +0700

    fix 2

--- routes/cart.ts
+++ routes/cart.ts
@@ -10,6 +10,6 @@ router
   .route("/user/:user_id")
   .get(cartController.getCartByUserId.bind(cartController));
 
-router.put("/:id", cartController.updateQuantity.bind(cartController));
+router.post("/:id", cartController.updateQuantity.bind(cartController));
 
 export default router;
